ead32b40d6b9190eb4d22bc4672d6e845a0f6485
martinarvidsson <martinarvidsson1994@gmail.com>
2015-10-18 17:19:48 +0000

    i can not seem to get the i have done a move button to work

--- Index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('Controller/Mastercontroller.php');
-session_start();
-$mc = new Mastercontroller;
-
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-$mc->Startapplication();

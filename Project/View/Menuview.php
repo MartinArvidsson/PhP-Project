a3bcc2099b7bf36f58e7c436bd11d0bf87575382
martinarvidsson <martinarvidsson1994@gmail.com>
2015-10-24 15:33:38 +0000

    started working on scoresystem

--- Project/View/Menuview.php
+++ Project/View/Menuview.php
@@ -6,13 +6,19 @@ class Menuview
     private static $Firsttofive ='Menuview::FT5';
     public function Response()
     {
+        //TODO : Hämta värden från model, skicka till GenerateMenu.
+        //TODO : Knapparna ska starta games
+        //TODO : Status på wins under games FT5/FT3.
         return $this->GenerateMenu();
     }
     
-    public function GenerateMenu()
+    public function GenerateMenu($PlayerXscore,$PlayerOscore)
     {
         return '
 			<form method="post" >
+			    <h1>Playerscores overall:</h1>
+			    <a>Player X : '.$PlayerXscore.'</a>
+			    <a>Player O : '.$PlayerOscore.'</a>
 				<fieldset>
 					<legend>Choose gamemode, First to 3 or First to 5</legend>
     				<input type="submit" name="' . self::$Firsttothree . '" value="Först till 3" />
